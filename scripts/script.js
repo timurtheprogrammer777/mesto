8227ae3a0286e07d0470f771966c3dd536b7d9be
Timur Enakaev <timur.enakaev.2000@gmail.com>
2023-01-19 20:31:42 +0300

    js fix done and project is ready for try #2

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,14 @@
-
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const popupButton = document.querySelector('.popup__button');
 
 const popupForm = document.querySelector('.popup__form');
-
 const formInputTitle = document.querySelector('#popup__input-title');
 const formInputSubtitle = document.querySelector('#popup__input-subtitle');
 
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-
-
 
 function openedPopup() {
     popup.classList.add('popup_opened');
@@ -24,12 +20,13 @@ function closedPopup() {
     popup.classList.remove('popup_opened');
 } // функция закрывает попап
 
-function getInput() {
+function getInput(evt) {
+    evt.preventDefault();
     profileTitle.textContent = formInputTitle.value;
     profileSubtitle.textContent = formInputSubtitle.value;
-} // функция
+    closedPopup();
+} // функция получает значение влью введенное в поля и заменяет текст классов profile__title и profile__subtitle
 
 buttonEdit.addEventListener('click', openedPopup);
-popupForm.addEventListener('click', getInput);
 popupClose.addEventListener('click', closedPopup);
-
+popupForm.addEventListener('submit', getInput);
